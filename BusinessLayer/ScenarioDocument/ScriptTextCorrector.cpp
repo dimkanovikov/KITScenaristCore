ba5b34f5547ca7855968eb309939c66327c9d0d6
Dimka Novikov <to@dimkanovikov.pro>
2017-11-26 23:59:15 +0300

    Implemented corrections for scene headings and scene characters

--- BusinessLayer/ScenarioDocument/ScriptTextCorrector.cpp
+++ BusinessLayer/ScenarioDocument/ScriptTextCorrector.cpp
@@ -12,6 +12,44 @@
 using BusinessLogic::ScenarioBlockStyle;
 using BusinessLogic::ScriptTextCorrector;
 
+namespace {
+    /**
+     * @brief Сместить блок в начало следующей страницы
+     * @param _cursor - курсор редактироуемого документа
+     * @param _block - блок для смещения
+     * @param _spaceToPageEnd - количество места до конца страницы
+     */
+    void moveBlockToNextPage(QTextCursor& _cursor, const QTextBlock& _block, qreal _spaceToPageEnd) {
+        //
+        // Смещаем курсор в начало блока
+        //
+        _cursor.setPosition(_block.position());
+        //
+        // Определим сколько пустых блоков нужно вставить
+        //
+        const QTextBlockFormat format = _block.blockFormat();
+        int insertBlockCount = _spaceToPageEnd
+                               / (format.lineHeight() + format.topMargin() + format.bottomMargin());
+        //
+        // Определим формат блоков декорации
+        //
+        QTextBlockFormat decorationFormat = format;
+        if (ScenarioBlockStyle::forBlock(_block) == ScenarioBlockStyle::SceneHeading) {
+            decorationFormat.setProperty(ScenarioBlockStyle::PropertyType, ScenarioBlockStyle::SceneHeadingShadow);
+        }
+        decorationFormat.setProperty(ScenarioBlockStyle::PropertyIsCorrection, true);
+        decorationFormat.setProperty(PageTextEdit::PropertyDontShowCursor, true);
+        //
+        // Вставляем блоки декорации
+        //
+        while (insertBlockCount-- > 0) {
+            _cursor.insertBlock();
+            _cursor.movePosition(QTextCursor::PreviousBlock);
+            _cursor.setBlockFormat(decorationFormat);
+        }
+    }
+}
+
 
 ScriptTextCorrector::ScriptTextCorrector(QTextDocument* _document) :
     QObject(_document),
@@ -157,78 +195,114 @@ void ScriptTextCorrector::correct()
                 (lastBlockHeight + blockHeight > pageHeight)
                 // но влезает хотя бы одна строка
                 && (lastBlockHeight + blockFormat.topMargin() + blockLineHeight < pageHeight);
-        if (atPageEnd
-            || atPageBreak) {
-            //
-            // Если это время и место, переносим на следующую страницу
-            //
-            if (ScenarioBlockStyle::forBlock(block) == ScenarioBlockStyle::SceneHeading) {
+        if (atPageEnd || atPageBreak) {
+            switch (ScenarioBlockStyle::forBlock(block)) {
                 //
-                // TODO: определить сколько строк нужно перенести
+                // Если это время и место
                 //
-                cursor.setPosition(block.position());
-                cursor.insertBlock();
-                cursor.movePosition(QTextCursor::PreviousBlock);
-                QTextBlockFormat format = block.blockFormat();
-                format.setProperty(ScenarioBlockStyle::PropertyType, ScenarioBlockStyle::SceneHeadingShadow);
-                format.setProperty(ScenarioBlockStyle::PropertyIsCorrection, true);
-                format.setProperty(PageTextEdit::PropertyDontShowCursor, true);
-                cursor.setBlockFormat(format);
+                case ScenarioBlockStyle::SceneHeading: {
+                    //
+                    // Переносим на следующую страницу
+                    //
+                    const qreal sizeToPageEnd = pageHeight - lastBlockHeight;
+                    ::moveBlockToNextPage(cursor, block, sizeToPageEnd);
+                    //
+                    // Обозначаем последнюю высоту, как высоту блока время и места
+                    //
+                    lastBlockHeight = blockHeight - blockFormat.topMargin();
+                    break;
+                }
 
-                lastBlockHeight = blockHeight - block.blockFormat().topMargin();
-            }
+                //
+                // Если это участники сцены
+                //
+                case ScenarioBlockStyle::SceneCharacters: {
+                    //
+                    // Определим предыдущий блок
+                    //
+                    QTextBlock previousBlock = block.previous();
+                    while (previousBlock.isValid() && !previousBlock.isVisible()) {
+                        previousBlock = previousBlock.previous();
+                    }
+                    //
+                    // Если перед ним идёт время и место, переносим его тоже
+                    //
+                    if (previousBlock.isValid()
+                        && ScenarioBlockStyle::forBlock(previousBlock) == ScenarioBlockStyle::SceneHeading) {
+                        const QTextBlockFormat previousBlockFormat = previousBlock.blockFormat();
+                        const qreal previousBlockHeight =
+                                previousBlockFormat.lineHeight() * previousBlock.layout()->lineCount()
+                                + previousBlockFormat.topMargin() + previousBlockFormat.bottomMargin();
+                        const qreal sizeToPageEnd = pageHeight - lastBlockHeight + previousBlockHeight;
+                        ::moveBlockToNextPage(cursor, previousBlock, sizeToPageEnd);
+                        //
+                        // Обозначаем последнюю высоту, как высоту блока время и места плюс высоту блока участников сцены
+                        //
+                        lastBlockHeight = previousBlockHeight - previousBlockFormat.topMargin() + blockHeight;
+                    }
+                    //
+                    // В противном случае, просто переносим блок на следующую страницу
+                    //
+                    else {
+                        const qreal sizeToPageEnd = pageHeight - lastBlockHeight;
+                        ::moveBlockToNextPage(cursor, block, sizeToPageEnd);
+                        //
+                        // Обозначаем последнюю высоту, как высоту блока время и места
+                        //
+                        lastBlockHeight = blockHeight - blockFormat.topMargin();
+                    }
 
-            //
-            // Если это участники сцены, переносим на следующую страницу
-            // - если перед ним идёт время и место, переносим его тоже
-            //
+                    break;
+                }
 
-            //
-            // Если это имя персонажа, переносим на следующую страницу
-            // - если перед именем идёт время и место, их переносим тоже
-            // - если перед именем идут участники сцены, их переносим тоже, и если перед ними
-            //   идёт время и место, его переносим тоже
-            //
+                //
+                // Если это имя персонажа, переносим на следующую страницу
+                // - если перед именем идёт время и место, их переносим тоже
+                // - если перед именем идут участники сцены, их переносим тоже, и если перед ними
+                //   идёт время и место, его переносим тоже
+                //
 
-            //
-            // Если это ремарка, переносим на следующую страницу
-            // - если перед ремаркой идёт имя персонажа переносим и его тоже
-            // - если перед ремаркой идёт реплика, то разрываем реплику, вставляя вместо ремарки
-            //   ДАЛЬШЕ и добавляя на новой странице имя персонажа с (ПРОД.)
-            //
+                //
+                // Если это ремарка, переносим на следующую страницу
+                // - если перед ремаркой идёт имя персонажа переносим и его тоже
+                // - если перед ремаркой идёт реплика, то разрываем реплику, вставляя вместо ремарки
+                //   ДАЛЬШЕ и добавляя на новой странице имя персонажа с (ПРОД.)
+                //
 
-            //
-            // Если это описание действия и попадает на разрыв
-            // - если на странице можно оставить текст, который займёт 2 и более строк,
-            //    оставляем максимум, а остальное переносим. Разрываем по предложениям
-            // - в остальном случае переносим полностью
-            // -- если перед описанием действия идёт время и место, переносим и его тоже
-            // -- если перед описанием действия идёт список участников, то переносим их
-            //	  вместе с предыдущим блоком время и место
-            //
+                //
+                // Если это описание действия и попадает на разрыв
+                // - если на странице можно оставить текст, который займёт 2 и более строк,
+                //    оставляем максимум, а остальное переносим. Разрываем по предложениям
+                // - в остальном случае переносим полностью
+                // -- если перед описанием действия идёт время и место, переносим и его тоже
+                // -- если перед описанием действия идёт список участников, то переносим их
+                //	  вместе с предыдущим блоком время и место
+                //
 
-            //
-            // Если это реплика или лирика и попадает на разрыв
-            // - если можно, то оставляем текст так, чтобы он занимал не менее 2 строк,
-            //	 добавляем ДАЛЬШЕ и на следующей странице имя персонажа с (ПРОД) и остальной текст
-            // - в противном случае
-            // -- если перед диалогом идёт имя персонажа, то переносим их вместе на след.
-            // -- если перед диалогом идёт ремарка
-            // --- если перед ремаркой идёт имя персонажа, то переносим их всех вместе
-            // --- если перед ремаркой идёт диалог, то разрываем по ремарке, пишем вместо неё
-            //	   ДАЛЬШЕ, а на следующей странице имя персонажа с (ПРОД), ремарку и сам диалог
-            //
+                //
+                // Если это реплика или лирика и попадает на разрыв
+                // - если можно, то оставляем текст так, чтобы он занимал не менее 2 строк,
+                //	 добавляем ДАЛЬШЕ и на следующей странице имя персонажа с (ПРОД) и остальной текст
+                // - в противном случае
+                // -- если перед диалогом идёт имя персонажа, то переносим их вместе на след.
+                // -- если перед диалогом идёт ремарка
+                // --- если перед ремаркой идёт имя персонажа, то переносим их всех вместе
+                // --- если перед ремаркой идёт диалог, то разрываем по ремарке, пишем вместо неё
+                //	   ДАЛЬШЕ, а на следующей странице имя персонажа с (ПРОД), ремарку и сам диалог
+                //
 
-            //
-            // В противном случае просто переходим на следующую страницу
-            //
-            else {
-                if (atPageEnd) {
-                    isBlockFirstOnPage = true;
-                    lastBlockHeight = 0;
-                } else {
-                    lastBlockHeight += blockHeight;
-                    lastBlockHeight -= pageHeight;
+                //
+                // В противном случае просто переходим на следующую страницу
+                //
+                default: {
+                    if (atPageEnd) {
+                        isBlockFirstOnPage = true;
+                        lastBlockHeight = 0;
+                    } else {
+                        lastBlockHeight += blockHeight;
+                        lastBlockHeight -= pageHeight;
+                    }
+                    break;
                 }
             }
         }
@@ -244,82 +318,4 @@ void ScriptTextCorrector::correct()
     }
 
     cursor.endEditBlock();
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-//    QAbstractTextDocumentLayout* layout = m_document->documentLayout();
-
-//    //
-//    // Формируем модель от заданной позиции до конца документа
-//    //
-//    m_blockItems.clear();
-//    QTextBlock block = m_document->begin();
-
-//    //
-//    // Если блок первый на странице, то не учитывается его верхний отступ
-//    //
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-//    while (block.isValid()) {
-//        h += height;
-
-//        const QRectF blockRect = layout->blockBoundingRect(block);
-//        const int startLineNumber = block.layout()->lineForTextPosition(block.position()).lineNumber();
-//        const int endLineNumber = block.layout()->lineForTextPosition(block.position() + block.length()).lineNumber();
-//        qDebug() << block.blockNumber() << height << h;
-//        m_blockItems.append(BlockItem{blockRect.height() +block.blockFormat().topMargin() + block.blockFormat().bottomMargin()});
-
-//        block = block.next();
-//    }
-
-//    //
-//    // Проходим документ и расставляем переносы
-//    //
-//    const QTextFrameFormat rootFrameFormat = m_document->rootFrame()->frameFormat();
-
-//    qDebug() << pageHeight;
-//    qreal currentYPosition = 0;
-//    for (int itemIndex = 0; itemIndex < m_blockItems.size(); ++itemIndex) {
-//        const BlockItem blockItem = m_blockItems.at(itemIndex);
-//        currentYPosition += blockItem.height;
-//        //
-//        // Если текущий блок попадает на разрыв страниц
-//        //
-//        if (currentYPosition > pageHeight) {
-//            qDebug() << itemIndex << "break";
-//        } else if (currentYPosition >= pageHeight+1) {
-//            qDebug() << itemIndex << "last";
-//        }
-
-
-//        if (currentYPosition >= pageHeight+1) {
-//            currentYPosition -= pageHeight;
-//        }
-//    }
 }
