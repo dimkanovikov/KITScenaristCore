51e234553c5d48e6862a2d78862a0c09c6a23642
Dimka Novikov <to@dimkanovikov.pro>
2019-10-13 01:51:35 +0300

    Small refactoring

--- 3rd_party/Widgets/CompletableTextEdit/CompletableTextEdit.cpp
+++ 3rd_party/Widgets/CompletableTextEdit/CompletableTextEdit.cpp
@@ -40,7 +40,7 @@ namespace {
      */
     class Completer : public QCompleter
     {
-	public:
+    public:
         explicit Completer(QObject* _p = 0) :
             QCompleter(_p),
             m_popup(new QListView),
@@ -69,11 +69,11 @@ namespace {
             setMaxVisibleItems(COMPLETER_MAX_ITEMS);
         }
 
-		/**
-		 * @brief Переопределяется для отображения подсказки по глобальной координате
-		 *		  левого верхнего угла области для отображения
-		 */
-		void completeReimpl(const QRect& _rect) {
+        /**
+         * @brief Переопределяется для отображения подсказки по глобальной координате
+         *		  левого верхнего угла области для отображения
+         */
+        void completeReimpl(const QRect& _rect) {
             Q_UNUSED(_rect);
 
             m_popup->setWindowFlags(Qt::Widget);
@@ -86,7 +86,7 @@ namespace {
             m_popup->setFixedWidth(m_popup->parentWidget()->width());
             m_popup->raise();
             WAF::Animation::sideSlideIn(m_popup, WAF::TopSide, false);
-		}
+        }
 
         /**
          * @brief Закрыть список автодополнения
@@ -103,10 +103,9 @@ namespace {
          * @brief Переопределяем, чтобы скрывать декорации, во время закрытия попапа
          */
         bool eventFilter(QObject* _watched, QEvent* _event) {
-            if ((_event->type() == QEvent::KeyPress
-                 || (_watched == m_popup && _event->type() == QEvent::Hide))
-                && m_popup->parentWidget() != nullptr) {
-                WAF::Animation::sideSlideOut(m_popup, WAF::TopSide, false);
+            if (_event->type() == QEvent::KeyPress
+                || (_watched == m_popup && _event->type() == QEvent::Hide)) {
+                closeCompleter();
             }
             return QCompleter::eventFilter(_watched, _event);
         }
@@ -121,7 +120,7 @@ namespace {
          * @brief Делегат для отрисовки элементов списка
          */
         QStyledItemDelegate* m_popupDelegate = nullptr;
-	};
+    };
 #else
     class Completer : public QCompleter
     {
@@ -153,104 +152,104 @@ CompletableTextEdit::CompletableTextEdit(QWidget* _parent) :
     m_useCompleter(true),
     m_completer(new Completer(this))
 {
-	m_completer->setWidget(this);
-	connect(m_completer, SIGNAL(activated(QString)), this, SLOT(applyCompletion(QString)));
+    m_completer->setWidget(this);
+    connect(m_completer, SIGNAL(activated(QString)), this, SLOT(applyCompletion(QString)));
 }
 
 void CompletableTextEdit::setUseCompleter(bool _use)
 {
-	if (m_useCompleter != _use) {
-		m_useCompleter = _use;
-	}
+    if (m_useCompleter != _use) {
+        m_useCompleter = _use;
+    }
 }
 
 QCompleter* CompletableTextEdit::completer() const
 {
-	return m_completer;
+    return m_completer;
 }
 
 bool CompletableTextEdit::isCompleterVisible() const
 {
-	return m_completer->popup()->isVisible();
+    return m_completer->popup()->isVisible();
 }
 
 bool CompletableTextEdit::complete(QAbstractItemModel* _model, const QString& _completionPrefix)
 {
-	bool success = false;
+    bool success = false;
 
-	if (m_useCompleter && canComplete()) {
-		if (_model != 0) {
-			//
-			// Настроим завершателя, если необходимо
-			//
-			bool settedNewModel = m_completer->model() != _model;
-			bool oldModelWasChanged = false;
-			if (!settedNewModel
-				&& _model != 0) {
-				oldModelWasChanged = m_completer->model()->rowCount() == _model->rowCount();
-			}
+    if (m_useCompleter && canComplete()) {
+        if (_model != 0) {
+            //
+            // Настроим завершателя, если необходимо
+            //
+            bool settedNewModel = m_completer->model() != _model;
+            bool oldModelWasChanged = false;
+            if (!settedNewModel
+                && _model != 0) {
+                oldModelWasChanged = m_completer->model()->rowCount() == _model->rowCount();
+            }
 
-			if (settedNewModel
-				|| oldModelWasChanged) {
-				m_completer->setModel(_model);
-				m_completer->setModelSorting(QCompleter::UnsortedModel);
-				m_completer->setCaseSensitivity(Qt::CaseInsensitive);
-			}
-			m_completer->setCompletionPrefix(_completionPrefix);
+            if (settedNewModel
+                || oldModelWasChanged) {
+                m_completer->setModel(_model);
+                m_completer->setModelSorting(QCompleter::UnsortedModel);
+                m_completer->setCaseSensitivity(Qt::CaseInsensitive);
+            }
+            m_completer->setCompletionPrefix(_completionPrefix);
 
-			//
-			// Если в модели для дополнения есть элементы
-			//
-			bool hasCompletions = m_completer->completionModel()->rowCount() > 0;
-			bool alreadyComplete = _completionPrefix.toLower().endsWith(m_completer->currentCompletion().toLower());
+            //
+            // Если в модели для дополнения есть элементы
+            //
+            bool hasCompletions = m_completer->completionModel()->rowCount() > 0;
+            bool alreadyComplete = _completionPrefix.toLower().endsWith(m_completer->currentCompletion().toLower());
 
-			if (hasCompletions
-				&& !alreadyComplete) {
-				m_completer->popup()->setCurrentIndex(
-							m_completer->completionModel()->index(0, 0));
+            if (hasCompletions
+                && !alreadyComplete) {
+                m_completer->popup()->setCurrentIndex(
+                            m_completer->completionModel()->index(0, 0));
 
-				//
-				// ... отобразим завершателя
-				//
-				QRect rect = cursorRect();
-				rect.moveLeft(rect.left() + verticalScrollBar()->width() + viewportMargins().left());
-				rect.moveTop(rect.top() + QFontMetricsF(currentCharFormat().font()).height());
-				rect.setWidth(
-							m_completer->popup()->sizeHintForColumn(0)
-							+ m_completer->popup()->verticalScrollBar()->sizeHint().width());
+                //
+                // ... отобразим завершателя
+                //
+                QRect rect = cursorRect();
+                rect.moveLeft(rect.left() + verticalScrollBar()->width() + viewportMargins().left());
+                rect.moveTop(rect.top() + QFontMetricsF(currentCharFormat().font()).height());
+                rect.setWidth(
+                            m_completer->popup()->sizeHintForColumn(0)
+                            + m_completer->popup()->verticalScrollBar()->sizeHint().width());
 
                 Completer* myCompleter = dynamic_cast<Completer*>(m_completer);
-				myCompleter->completeReimpl(rect);
-				emit popupShowed();
+                myCompleter->completeReimpl(rect);
+                emit popupShowed();
 
-				success = true;
-			}
-		}
+                success = true;
+            }
+        }
 
-		if (!success) {
-			//
-			// ... скроем, если был отображён
-			//
-			closeCompleter();
-		}
-	}
+        if (!success) {
+            //
+            // ... скроем, если был отображён
+            //
+            closeCompleter();
+        }
+    }
 
-	return success;
+    return success;
 }
 
 void CompletableTextEdit::applyCompletion()
 {
-	if (isCompleterVisible()) {
-		//
-		// Получим выбранный из списка дополнений элемент
-		//
-		QModelIndex currentIndex = m_completer->popup()->currentIndex();
-		QString completion = m_completer->popup()->model()->data(currentIndex).toString();
+    if (isCompleterVisible()) {
+        //
+        // Получим выбранный из списка дополнений элемент
+        //
+        QModelIndex currentIndex = m_completer->popup()->currentIndex();
+        QString completion = m_completer->popup()->model()->data(currentIndex).toString();
 
-		applyCompletion(completion);
+        applyCompletion(completion);
 
-		closeCompleter();
-	}
+        closeCompleter();
+    }
 }
 
 void CompletableTextEdit::applyCompletion(const QString& _completion)
@@ -265,16 +264,16 @@ void CompletableTextEdit::applyCompletion(const QString& _completion)
     QApplication::inputMethod()->commit();
 #endif
 
-	while (!textCursor().atBlockEnd()) {
-		QTextCursor cursor = textCursor();
-		cursor.movePosition(QTextCursor::StartOfBlock, QTextCursor::KeepAnchor);
-		if (cursor.selectedText().endsWith(m_completer->completionPrefix(), Qt::CaseInsensitive)) {
-			break;
-		}
-		moveCursor(QTextCursor::NextCharacter);
-	}
+    while (!textCursor().atBlockEnd()) {
+        QTextCursor cursor = textCursor();
+        cursor.movePosition(QTextCursor::StartOfBlock, QTextCursor::KeepAnchor);
+        if (cursor.selectedText().endsWith(m_completer->completionPrefix(), Qt::CaseInsensitive)) {
+            break;
+        }
+        moveCursor(QTextCursor::NextCharacter);
+    }
 
-	textCursor().insertText(textToInsert);
+    textCursor().insertText(textToInsert);
 
 #ifdef MOBILE_OS
     if (!textToInsert.endsWith(" ")) {
@@ -293,5 +292,5 @@ void CompletableTextEdit::closeCompleter()
 
 bool CompletableTextEdit::canComplete() const
 {
-	return true;
+    return true;
 }
