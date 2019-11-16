abc698d9eaf407a0a181f5de0dafec0956825887
Dimka Novikov <to@dimkanovikov.pro>
2019-11-16 23:34:39 +0200

    Fixed crash on printing script after print preview

--- BusinessLayer/Export/PdfExporter.cpp
+++ BusinessLayer/Export/PdfExporter.cpp
@@ -480,7 +480,7 @@ namespace {
     /**
      * @brief Прокрутить диалог предпросмотра к заданной позиции
      */
-    static bool setPrintPreviewScrollValue(const QPointer<QPrintPreviewDialog>& _dialog, int _value) {
+    static bool setPrintPreviewScrollValue(QSharedPointer<QPrintPreviewDialog> _dialog, int _value) {
         if (_dialog.isNull()) {
             return false;
         }
@@ -498,7 +498,7 @@ namespace {
     /**
      * @brief Получить позиции прокрутки диалога предпросмотра печати
      */
-    static int printPreviewScrollValue(const QPointer<QPrintPreviewDialog>& _dialog) {
+    static int printPreviewScrollValue(QSharedPointer<QPrintPreviewDialog> _dialog) {
         if (_dialog.isNull()) {
             return 0;
         }
@@ -695,15 +695,15 @@ void PdfExporter::printPreview(ScenarioDocument* _scenario, const ExportParamete
     //
     // Настроим диалог предварительного просмотра
     //
-    QPointer<QPrintPreviewDialog> printDialog = new QPrintPreviewDialog(printer.data(), qApp->activeWindow());
+    QSharedPointer<QPrintPreviewDialog> printDialog(new QPrintPreviewDialog(printer.data(), qApp->activeWindow()));
     printDialog->setWindowState(Qt::WindowMaximized);
     connect(printDialog.data(), &QPrintPreviewDialog::paintRequested, this, &PdfExporter::aboutPrint);
     if (m_lastScriptPreviewScrollPosition.first == _scenario) {
         //
         // Если осуществляется повторный предпросмотр документа, пробуем восстановить положение полосы прокрутки
         //
-        connect(this, &PdfExporter::printed, printDialog.data(), [&printDialog] {
-            QTimer::singleShot(300, [&printDialog] {
+        connect(this, &PdfExporter::printed, printDialog.data(), [printDialog] {
+            QTimer::singleShot(300, [printDialog] {
                 setPrintPreviewScrollValue(printDialog, m_lastScriptPreviewScrollPosition.second);
             });
         });
@@ -720,6 +720,7 @@ void PdfExporter::printPreview(ScenarioDocument* _scenario, const ExportParamete
     // Сохраняем позицию прокрутки
     //
     m_lastScriptPreviewScrollPosition.second = printPreviewScrollValue(printDialog);
+    printDialog->disconnect();
 }
 
 void PdfExporter::printPreview(const ResearchModelCheckableProxy* _researchModel, const ExportParameters& _exportParameters)
@@ -744,7 +745,7 @@ void PdfExporter::printPreview(const ResearchModelCheckableProxy* _researchModel
     //
     // Настроим диалог предварительного просмотра
     //
-    QPointer<QPrintPreviewDialog> printDialog = new QPrintPreviewDialog(printer.data(), qApp->activeWindow());
+    QSharedPointer<QPrintPreviewDialog> printDialog(new QPrintPreviewDialog(printer.data(), qApp->activeWindow()));
     printDialog->setWindowState(Qt::WindowMaximized);
     connect(printDialog.data(), &QPrintPreviewDialog::paintRequested, this, &PdfExporter::aboutPrint);
 
@@ -757,6 +758,7 @@ void PdfExporter::printPreview(const ResearchModelCheckableProxy* _researchModel
     // Сохраняем позицию прокрутки
     //
     m_lastScriptPreviewScrollPosition.second = printPreviewScrollValue(printDialog);
+    printDialog->disconnect();
 }
 
 void PdfExporter::aboutPrint(QPrinter* _printer)
