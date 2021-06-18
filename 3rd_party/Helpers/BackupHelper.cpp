46249e6bf6a22affa343aec22ed14be72ff1ec23
dimkanovikov <to@dimkanovikov.pro>
2021-06-18 11:08:22 +0300

    Extend BackupHelper api to use it in mobile version

--- 3rd_party/Helpers/BackupHelper.cpp
+++ 3rd_party/Helpers/BackupHelper.cpp
@@ -5,6 +5,8 @@
 
 #include <Domain/Scenario.h>
 
+#include <3rd_party/Helpers/RunOnce.h>
+
 #include <QDateTime>
 #include <QDir>
 #include <QFileInfo>
@@ -32,7 +34,13 @@ void BackupHelper::setIsActive(bool _isActive)
 {
 	if (m_isActive != _isActive) {
 		m_isActive = _isActive;
-	}
+    }
+}
+
+void BackupHelper::configure(bool _fullBackup, bool _versions)
+{
+    m_isSaveFullBackup = _fullBackup;
+    m_isSaveVersions = _versions;
 }
 
 void BackupHelper::setBackupDir(const QString& _dir)
@@ -44,72 +52,76 @@ void BackupHelper::setBackupDir(const QString& _dir)
 
 void BackupHelper::saveBackup(const QString& _filePath, const QString& _newName)
 {
-	if (m_isActive
-		&& !m_isInProgress) {
-		m_isInProgress = true;
+    if (!m_isActive) {
+        return;
+    }
 
+    const auto canRun = RunOnce::tryRun(Q_FUNC_INFO);
+    if (!canRun) {
+        return;
+    }
+
+    //
+    // FIXME: Переделать через Project::backupFilePath
+    //
+
+    //
+    // Создаём папку для хранения резервных копий, если такой ещё нет
+    //
+    QDir::root().mkpath(m_backupDir);
+
+    //
+    // Сформируем путь к резервной копии
+    //
+    QString backupPath = m_backupDir;
+    if (!backupPath.endsWith(QDir::separator())) {
+        backupPath.append(QDir::separator());
+    }
+
+    QFileInfo fileInfo(_filePath);
+    const QString backupBaseName = _newName.isEmpty() ? fileInfo.completeBaseName() : _newName;
+    const QString tmpBackupFileName =
+            QString("%1%2.backup.tmp.%3").arg(backupPath, backupBaseName, fileInfo.completeSuffix());
+    const QString backupFileName =
+            QString("%1%2.full.backup.%3").arg(backupPath, backupBaseName, fileInfo.completeSuffix());
+    const QString backupVersionsFileName =
+            QString("%1%2.versions.backup.%3").arg(backupPath, backupBaseName, kBackupVersionsExtension);
+
+    if (m_isSaveFullBackup) {
         //
-        // FIXME: Переделать через Project::backupFilePath
+        // Копируем файл во временную резервную копию
         //
+        if (QFile::copy(_filePath, tmpBackupFileName)) {
+            //
+            // Если скопировать удалось, переименовываем временную копию
+            //
+            QFile::remove(backupFileName);
+            QFile::rename(tmpBackupFileName, backupFileName);
+        }
+    }
 
-		//
-		// Создаём папку для хранения резервных копий, если такой ещё нет
-		//
-		QDir::root().mkpath(m_backupDir);
-
-		//
-		// Сформируем путь к резервной копии
-		//
-		QString backupPath = m_backupDir;
-		if (!backupPath.endsWith(QDir::separator())) {
-			backupPath.append(QDir::separator());
-		}
-
-		QFileInfo fileInfo(_filePath);
-        const QString backupBaseName = _newName.isEmpty() ? fileInfo.completeBaseName() : _newName;
-        const QString tmpBackupFileName =
-                QString("%1%2.backup.tmp.%3").arg(backupPath, backupBaseName, fileInfo.completeSuffix());
-		const QString backupFileName =
-                QString("%1%2.full.backup.%3").arg(backupPath, backupBaseName, fileInfo.completeSuffix());
-		const QString backupVersionsFileName =
-                QString("%1%2.versions.backup.%3").arg(backupPath, backupBaseName, kBackupVersionsExtension);
-
-		//
-		// Копируем файл во временную резервную копию
-		//
-		if (QFile::copy(_filePath, tmpBackupFileName)) {
-			//
-			// Если скопировать удалось, переименовываем временную копию
-			//
-			QFile::remove(backupFileName);
-			QFile::rename(tmpBackupFileName, backupFileName);
-		}
-
-		//
-		// Добавляем версию сценария в файл с резервными копиями версий текста сценария
-		//
+    //
+    // Добавляем версию сценария в файл с резервными копиями версий текста сценария
+    //
+    if (m_isSaveVersions){
         {
-			{
-                QSqlDatabase db = QSqlDatabase::addDatabase("QSQLITE", kBackupDbConnectionName);
-				db.setDatabaseName(backupVersionsFileName);
-				db.open();
-				//
-				// Храним всего 100 копий, удаляя более старые
-				//
-				QSqlQuery backuper(db);
-				backuper.exec("CREATE TABLE IF NOT EXISTS versions (id INTEGER, version TEXT NOT NULL, datetime TEXT NOT NULL)");
-				backuper.exec("UPDATE versions SET id = (id + 1)");
-				backuper.exec("DELETE FROM versions WHERE id = 101");
+            QSqlDatabase db = QSqlDatabase::addDatabase("QSQLITE", kBackupDbConnectionName);
+            db.setDatabaseName(backupVersionsFileName);
+            db.open();
+            //
+            // Храним всего 100 копий, удаляя более старые
+            //
+            QSqlQuery backuper(db);
+            backuper.exec("CREATE TABLE IF NOT EXISTS versions (id INTEGER, version TEXT NOT NULL, datetime TEXT NOT NULL)");
+            backuper.exec("UPDATE versions SET id = (id + 1)");
+            backuper.exec("DELETE FROM versions WHERE id = 101");
 
-				backuper.prepare("INSERT INTO versions VALUES(1, ?, ?)");
-				backuper.addBindValue(DataStorageLayer::StorageFacade::scenarioStorage()->current()->text());
-				backuper.addBindValue(QDateTime::currentDateTime().toString(Qt::ISODate));
-				backuper.exec();
-			}
+            backuper.prepare("INSERT INTO versions VALUES(1, ?, ?)");
+            backuper.addBindValue(DataStorageLayer::StorageFacade::scenarioStorage()->current()->text());
+            backuper.addBindValue(QDateTime::currentDateTime().toString(Qt::ISODate));
+            backuper.exec();
+        }
 
-            QSqlDatabase::removeDatabase(kBackupDbConnectionName);
-		}
-
-		m_isInProgress = false;
-	}
+        QSqlDatabase::removeDatabase(kBackupDbConnectionName);
+    }
 }
